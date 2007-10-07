6bb90d1255d84c06190b7dec67cc0077069f2ac5
Werner Lemberg <wl@rigel.site>
2007-10-07 22:06:45 +0200

    Zwei neue Skripte zur Filterung von `wortliste'.

---
+++ extract-tex-new.pl
@@ -0,0 +1,41 @@
+#! /usr/bin/perl -w
+#
+# extract-tex-new.pl
+#
+# Dieses Perl-Skript extrahiert aus der `wortliste'-Datei eine Eingabedatei
+# für Patgen, und zwar für die neue deutsche Rechtschreibung.
+#
+# Aufruf:  perl extract-tex-new.pl < wortliste > input.patgen
+
+use strict;
+use locale;
+use POSIX qw(locale_h);
+
+my $prog = $0;
+$prog =~ s@.*/@@;
+
+setlocale(LC_CTYPE, "de_DE.ISO8859-1");
+setlocale(LC_COLLATE, "de_DE.ISO8859-1");
+
+while (<>) {
+  chop;
+  next if /^#/;
+
+  my @feld = split(';');
+  next if $#feld < 0;
+
+  # Felder 2, 4, 5 und 7
+  my $zeile = $feld[1];
+  $zeile = $feld[3] if defined $feld[3] && $feld[3] ne "---";
+  $zeile = $feld[4] if defined $feld[4] && $feld[4] ne "---";
+  $zeile = $feld[6] if defined $feld[6] && $feld[6] ne "---";
+  next if $zeile eq "---";
+
+  # entferne Doppeldeutigkeiten ganz
+  next if /[\[\]]/;
+  # entferne Markierungen für schlechte Trennungen
+  $zeile =~ s/\.//g;
+  print "$zeile\n";
+}
+
+# eof
---
+++ extract-tex-old.pl
@@ -0,0 +1,43 @@
+#! /usr/bin/perl -w
+#
+# extract-tex-old.pl
+#
+# Dieses Perl-Skript extrahiert aus der `wortliste'-Datei eine Eingabedatei
+# für Patgen, und zwar für die alte deutsche Rechtschreibung.
+#
+# Aufruf:  perl extract-tex-alt.pl < wortliste > input.patgen
+
+use strict;
+use locale;
+use POSIX qw(locale_h);
+
+my $prog = $0;
+$prog =~ s@.*/@@;
+
+setlocale(LC_CTYPE, "de_DE.ISO8859-1");
+setlocale(LC_COLLATE, "de_DE.ISO8859-1");
+
+while (<>) {
+  chop;
+  next if /^#/;
+
+  my @feld = split(';');
+  next if $#feld < 0;
+
+  # Felder 2, 3, 5 und 6
+  my $zeile = $feld[1];
+  $zeile = $feld[2] if defined $feld[2] && $feld[2] ne "---";
+  $zeile = $feld[4] if defined $feld[4] && $feld[4] ne "---";
+  $zeile = $feld[5] if defined $feld[5] && $feld[5] ne "---";
+  next if $zeile eq "---";
+
+  # entferne Doppeldeutigkeiten ganz
+  next if /[\[\]]/;
+  # entferne spezielle Trennungen
+  $zeile =~ s/\{(.*?)\|.*?\}/$1/g;
+  # entferne Markierungen für schlechte Trennungen
+  $zeile =~ s/\.//g;
+  print "$zeile\n";
+}
+
+# eof
