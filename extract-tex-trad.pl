7e4c0805f7811fa71ca7a3e13f16edfc38a09628
Werner Lemberg <wl@rigel.site>
2008-10-07 12:18:24 +0200

    Redefinition von Feld 8.
    Feld 8 enthält nun die deutschschweizerische Variante.
    Als Kompensation treten Felder 6, 7 und 8 stets gemeinsam auf
    (wenn Feld 5 leer ist).

--- extract-tex-trad.pl
+++ extract-tex-trad.pl
@@ -25,12 +25,11 @@ while (<>) {
   my @feld = split(';');
   next if $#feld < 0;
 
-  # Felder 2, 3, 5, 6 und 8
+  # Felder 2, 3, 5 und 6
   my $zeile = $feld[1];
   $zeile = $feld[2] if defined $feld[2] && $feld[2] ne "-3-";
   $zeile = $feld[4] if defined $feld[4] && $feld[4] ne "-5-";
   $zeile = $feld[5] if defined $feld[5] && $feld[5] ne "-6-";
-  $zeile = $feld[7] if defined $feld[7] && $feld[7] ne "-8-";
   next if $zeile eq "-2-";
 
   # entferne spezielle Trennungen
