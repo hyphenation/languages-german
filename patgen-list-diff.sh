514e33209f10afac5ab9162d633bf17361399f09
Stephan Hennig <stephanhennig@arcor.de>
2012-04-19 11:35:26 +0200

    Skript: Normalisiere Git-Commit-Hash.

--- patgen-list-diff.sh
+++ patgen-list-diff.sh
@@ -1,15 +1,20 @@
 #!/bin/sh
 #
 # Dieses Skript erzeugt Differenzbilder (diff) für die
-# Patgen-Eingabelisten zwischen einem angegebenen Commit und "master".
-# Die Ausgabedateien werden als dehyph*-x/<commit hash>.diff in
-# Verzeichnissen gespeichert, die der jeweiligen Rechtschreibung
-# entsprechen.
+# Patgen-Eingabelisten zwischen einem angegebenen Commits und "master".
+# Die Ausgabedateien werden als Dateien
 #
-# Aufruf:  sh patgen-list-diff <commit hash>
+#   dehyph*-x/<commit hash>.diff
+#
+# in Verzeichnissen gespeichert, die der jeweiligen Rechtschreibung
+# entsprechen.  Der Commit kann in jeder gültigen Git-Syntax angegeben
+# werden.  Für die Dateinamen wird der entsprechende abgekürzte
+# alphanumerische Commit-Hash verwendet.
+#
+# Aufruf:  sh patgen-list-diff.sh <commit>
 #
 #
-# Eingabe:  <commit hash>          Ein Git-Commit-Hash.
+# Eingabe:  <commit>               Ein Git-Commit.
 #
 # Ausgabe:
 #   dehyphn-x/<commit hash>.diff   Differenzbild refromierte Rechtschreibung
@@ -17,18 +22,22 @@
 #   dehyphts-x/<commit hash>.diff  Differenzbild traditionelle Rechtschreibung
 #                                  in der Schweiz.
 #
-# Achtung: Der <commit hash> sollte nur Zeichen enthalten, welche in
-#          Dateinamen zulässig sind!
-#
-
 if test $# -eq 0
 then
-  echo 'usage: patgen-list-diff <commit hash>'
-  echo 'Create diffs for patgen input lists between <commit hash> and "master" and'
+  echo 'usage: patgen-list-diff <commit>'
+  echo 'Create diffs for patgen input lists between <commit> and "master" and'
   echo 'save them as files dehyph*-x/<commit hash>.diff in directories'
   echo 'corresponding to the spelling.'
   exit 1
 fi
+FROMCOMMIT=$1
+FROMHASH=$(git log -1 --format=%h $FROMCOMMIT)
+if test -z $FROMHASH
+then
+  echo 'patgen-list-diff.sh: error identifying commit hash: ' $FROMCOMMIT
+  exit 1
+fi
+echo "Diff'ing patgen input files between commits $FROMHASH ($FROMCOMMIT) and master."
 TEMPSCRIPT=temp-patgen-list-diff.sh
 echo 'if git checkout $1' > $TEMPSCRIPT
 echo 'then' >> $TEMPSCRIPT
@@ -47,4 +56,4 @@ echo '  diff dehypht-x/words.hyphenated.trad.$1 dehypht-x/words.hyphenated.trad 
 echo '  diff dehyphts-x/words.hyphenated.swiss.$1 dehyphts-x/words.hyphenated.swiss > dehyphts-x/$1.diff' >> $TEMPSCRIPT
 echo 'fi' >> $TEMPSCRIPT
 echo 'exec rm -f ' $TEMPSCRIPT >> $TEMPSCRIPT
-exec $TEMPSCRIPT $1
+exec $TEMPSCRIPT $FROMHASH
