3daf489fb190030c5be9288c51d93f460a3c3cbd
Stephan Hennig <sh2d@arcor.de>
2013-11-23 20:00:05 +0100

    Konsistenzpruefung: Refactor: Ueberarbeite helper_words.lua.
    * Verbessere Kommentare.
    
    * Einige interne Bezeichner wurden unbenannt.
    
    * Geringfuegige Aenderungen an der Grammatik.
    
    * Keine funktionalen Aenderungen.

--- skripte/lua/helper_words.lua
+++ skripte/lua/helper_words.lua
@@ -70,70 +70,80 @@ local Usub = unicode.utf8.sub
 
 
 --
--- Prüfmuster für Wortstruktur
+-- Hilfsfunktionen und -variablen für LPEG-Mustersuche.
 --
-local word_property = {}
+-- Eine Tabelle, in der während der Mustersuche Eigenschaften des
+-- untersuchten Wortes gespeichert werden.
+local word_property
 --
---- <strong>nicht-öffentlich</strong> Lösche alle Worteigenschaften.
+--- <strong>nicht-öffentlich</strong> Diese Funktion wird zu Beginn
+--- einer erfolgreichen Prüfung der Wortgrammatik gegen einen String
+--- ausgeführt.
+-- Initialisiert Tabelle mit Worteigenschaften.
+local function _at_word_start()
+   word_property = {}
+end
 --
--- @return Tabelle <code>word_property</code>
-local function _init_word_prop()
-   word_property.has_invalid_alt = false
-   word_property.has_nonstd = false
-   word_property.has_nonstd_sss = false
-   word_property.has_eszett = false
+--- <strong>nicht-öffentlich</strong> Diese Funktion wird am Ende einer
+--- erfolgreichen Prüfung der Wortgrammatik gegen einen String
+--- ausgeführt.
+--  Verwirft alle ausstehenden Captures.
+--
+-- @param ... Variable Anzahl an Strings
+--
+-- @return Tabelle mit Worteigenschaften
+local function _at_word_end(...)
    return word_property
 end
 --
 --- <strong>nicht-öffentlich</strong> Merke Eigenschaft
---- 'Spezialtrennung gefunden'.
+--- 'Spezialtrennung enthalten'.
+-- Die Capture wird nicht verändert.
 --
 -- @param c Capture
 --
 -- @return Capture
-local function _word_prop_has_nonstd(c)
+local function _property_has_nonstd(c)
    word_property.has_nonstd = true
    return c
 end
 --
 --- <strong>nicht-öffentlich</strong> Merke Eigenschaft
---- 'Dreikonsonantenregel für s gefunden'.
+--- 'Dreikonsonantenregel für s enthalten'.
+-- Die Capture wird nicht verändert.
 --
 -- @param c Capture
 --
 -- @return Capture
-local function _word_prop_has_nonstd_sss(c)
+local function _property_has_nonstd_sss(c)
    word_property.has_nonstd_sss = true
    return c
 end
 --
---- <strong>nicht-öffentlich</strong> Merke Eigenschaft
---- 'ß gefunden'.
---
--- @param c Capture
---
--- @return Capture
-local function _word_prop_has_eszett(c)
+--- <strong>nicht-öffentlich</strong> Merke Eigenschaft 'ß enthalten'.
+local function _property_has_eszett()
    word_property.has_eszett = true
-   return c
 end
 --
---- <strong>nicht-öffentlich</strong> Diese Funktion wird in einer
---- Funktionscapture genutzt, um Captures zusammenzufügen.
+--- <strong>nicht-öffentlich</strong> Füge eine beliebige Zahl von
+--- Strings zusammen.
+-- Diese Funktion wird in einer Funktionscapture genutzt, um Cluster
+-- zusammenzufügen.
 --
--- @param ... eine beliebige Zahl von Captures
+-- @param ... Variable Anzahl an Strings
 --
--- @return Ergebniscapture (String)
+-- @return zusammengesetzter String
 local function _concatenate_captures(...)
 --   io.stderr:write("conc: ",Tconcat({...}, ","),"\n")-- For debugging purposes.
    return Tconcat({...})
 end
 --
---- <strong>nicht-öffentlich</strong> Diese Funktion wird in einer
---- Funktionscapture verwendet, um die Gleichheit zweier Captures
---- festzustellen.  Falls zwei Captures ungleich sind, so wird dies in
---- Tabelle <code>word_property</code> vermerkt.  Die zweite Capture
---- wird immer verworfen.
+--- <strong>nicht-öffentlich</strong> Prüfe zwei Alternativen auf
+--- Gleichheit.
+-- Diese Funktion wird in einer Funktionscapture verwendet, um die
+-- Gleichheit zweier Alternativen zu prüfen.  Falls die Alternativen
+-- ungleich sind, so wird dies als Worteigenschaft vermerkt.  Ergebnis
+-- ist das erste Argument.
 --
 -- @param a Capture 1
 --
@@ -144,42 +154,132 @@ local function _is_equal_two_alternatives(a, b)
    word_property.has_invalid_alt = word_property.has_invalid_alt or (a ~= b)
    return a
 end
+
+
+
 --
--- Grammatik für ein Wort.
+-- Die Grammatik eines Wortes.
 --
--- Diese Grammatik prüft nicht, ob die ersten und letzten beiden Zeichen
--- eines Wortes Buchstaben sind.  Die vollständige Gültigkeit von
--- Wörtern sollte mit zusätzlichen, nachgeschalteten Prüfungen erfolgen.
+-- Diese Grammatik beschreibt die Struktur zulässiger Wörter.  Durch
+-- Prüfen eines Strings gegen diese Grammatik kann festgestellt werden,
+-- ob dieser ein Wort im Sinne der Grammatik enthält.
+--
+-- Bei positivem Ergebnis ist der Rückgabewert der Funktion `match` eine
+-- Tabelle, in welcher während der Prüfung die folgenden Eigenschaften
+-- des betrachteten Wortes gespeichert werden.
+--
+--    Feld                 Bedeutung
+--
+--    has_invalid_alt      Alternativen sind nicht identisch
+--    has_eszett           Wort enthält Buchstaben 'ß'
+--    has_nonstd           Wort enthält Spezialtrennung
+--    has_nonstd_sss       Wort enthält Spezialtrennung für das 's'
+--
+-- Die Grammatik beschreibt die Zulässigkeit von Wörtern nicht
+-- erschöpfend.  So wird beispielsweise nicht darauf eingegangen, ob
+-- Trennstellen einen Mindestabstand vom Wortrand haben müssen.  Die
+-- Zulässigkeit von Wörtern sollten daher mit zusätzlichen,
+-- nachgeschalteten Prüfungen festgestellt werden (siehe Funktion
+-- `validate_word()`).
+--
+-- Weitere Informationen können der Datei `README.wortliste` entnommen
+-- werden.
 --
 local word = P{
    -- Initialregel.
-   "word_with_property_table",
+   "property_table",
    --
    -- Wort
    --
-   -- Während des Mustervergleichs werden verschiedene Eigenschaften des
-   -- betrachteten Wortes in einer modullokalen Tabelle gespeichert.
-   -- Die Flags innerhalb dieser Tabelle werden vor dem Mustervergleich
-   -- gelöscht.
-   word_with_property_table = P"" / _init_word_prop * V"word" * -1,
-   -- Ein Wort ist ein Teilwort, kann also führende oder abschließende
-   -- Trennzeichen enthalten.  Diese Generalisierung vereinfacht die
-   -- Grammatik im Zusammenhang mit Alternativen etwas.
-   word = V"part_word" * -1,
+   -- Am Anfang der Prüfung wird die Tabelle mit Worteigenschaften
+   -- zurückgesetzt.  Am Ende werden alle ausstehenden Captures
+   -- verworfen.
+   property_table = P(true) / _at_word_start * V"word" * -1 / _at_word_end,
+   -- Ein Wort ist ein Teilwort, kann also führende und abschließende
+   -- Trennstellenmarkierungen enthalten.  Diese Generalisierung
+   -- vereinfacht die Grammatik im Zusammenhang mit Alternativen etwas.
+   word = V"part_word",
+   --
+   -- Teilwort
+   --
    -- Ein Teilwort besteht aus zusammenhängenden Zeichenketten,
-   -- sogenannten Klustern, die durch Trennzeichen voneinander getrennt
-   -- sind.  Ein Teilwort kann ein optionales führendes und schließendes
-   -- Trennzeichen enthalten.  Die Capture enthält das Wort ohne
-   -- jegliche Trennzeichen.
+   -- sogenannten Klustern, die durch Trennstellenmarkierungen
+   -- voneinander getrennt sind.  Ein Teilwort kann eine führende und
+   -- abschließende Trennstellenmarkierung enthalten.  Die Capture
+   -- enthält die Zeichen aller Kluster (die Buchstaben des Teilwortes
+   -- ohne jegliche Trennzeichen).
    part_word = V"ophyphen" * (V"cluster" * (V"hyphen" * V"cluster")^0) / _concatenate_captures * V"ophyphen",
-   -- Ein Kluster besteht aus mehreren aneinandergereihten fundamentalen
-   -- Klustern ohne Unterbrechung durch Trennzeichen.  Es gibt drei
-   -- Arten von fundamentalen Klustern: Buchstabenkluster,
-   -- Spezialtrennungen und Alternativen:
+   --
+   -- Trennstellen
+   --
+   -- Die Markierung von Trennstellen erfolgt zunächst nach deren
+   -- morphologischer Struktur.  Wahlweise kann auch eine (zusätzliche)
+   -- qualitative Bewertung erfolgen.
+   --
+   -- morphologische Markierungen
+   --
+   -- innerhalb von Wortstämmen oder vor Suffixen
+   hyphen_inner = V"hyphen_ch_inner"^1,
+   -- nach Präfixen oder Verbalpartikeln
+   hyphen_prefix = V"hyphen_ch_prefix"^1,
+   -- an Wortfugen
+   hyphen_compound = V"hyphen_ch_compound"^1,
+   -- nach Präfix eines zusammengesetzten Wortes
+   hyphen_compound_prefix = V"hyphen_ch_prefix" * V"hyphen_compound",
+   -- vor Suffix eines zusammengesetzten Wortes
+   hyphen_compound_suffix = V"hyphen_ch_inner" * V"hyphen_compound",
+   -- eine morphologische Markierung (Achtung: In der folgenden Regel
+   -- ist die Reihenfolge der Prüfung relevant.  Gemischte Trennzeichen
+   -- müssen vor reinen Trennzeichen geprüft werden.)
+   hyphen_morph =
+      V"hyphen_compound_prefix"
+      + V"hyphen_compound_suffix"
+      + V"hyphen_inner"
+      + V"hyphen_prefix"
+      + V"hyphen_compound",
+   --
+   -- qualitative Markierungen (Bewertung)
+   --
+   -- eine Bewertung
+   hyphen_quality = V"hyphen_ch_quality" * V"hyphen_ch_quality"^-2,
+   -- eine optionale Bewertung
+   hyphen_opt_quality = V"hyphen_quality"^-1,
+   --
+   -- unkategorisierte Markierungen
+   --
+   hyphen_uncategorized = V"hyphen_ch_uncategorized",
+   --
+   -- Eine beliebige Trennstellenmarkierung ist entweder unkategorisiert
+   -- oder morphologisch markiert mit optional folgender Bewertung.  Die
+   -- Bewertung kann auch alleinstehen.
+   hyphen = V"hyphen_uncategorized"
+      + V"hyphen_morph" * V"hyphen_opt_quality"
+      + V"hyphen_quality"
+,
+   --
+   -- Eine optionale Trennstellenmarkierung.
+   ophyphen = V"hyphen"^-1,
+   --
+   -- Die folgenden Zeichen werden zur Trennstellenmarkierung verwendet:
+   hyphen_ch_inner = P"-",
+   hyphen_ch_prefix = P"|",
+   hyphen_ch_compound = P"=",
+   hyphen_ch_quality = P".",
+   hyphen_ch_uncategorized = P"·",
+   --
+   -- Kluster
+   --
+   -- Ein Kluster besteht aus einem oder mehreren aufeinanderfolgenden
+   -- `fundamentalen Klustern` ohne Unterbrechung durch Trennzeichen.
+   -- Es gibt drei Arten von fundamentalen Klustern:
+   --
+   --   * Buchstabenkluster,
+   --   * Ausdrücke für Alternativen,
+   --   * Ausdrücke für Spezialtrennungen,
    --
    --                 Bü-cher   E{ck/k-k}e   Sto{ff/ff-f}et-zen   Wach[-s/s-]tu-be
-   -- fund. Kluster   11-2222   1222222223   11122222222233-444   1111222222233-44
    -- Kluster         11-2222   1111111111   11111111111111-222   1111111111111-22
+   -- fund. Kluster   11-2222   1222222223   11122222222233-444   1111222222233-44
    --
    cluster = (V"cl_letter" + V"cl_nonstd" + V"cl_alt")^1,
    --
@@ -193,7 +293,7 @@ local word = P{
    + P"Ä"
    + P"Ö"
    + P"Ü"
-   + P"ß" / _word_prop_has_eszett
+   + P"ß" / _property_has_eszett
    + P"à"
    + P"á"
    + P"â"
@@ -212,7 +312,33 @@ local word = P{
    + P"ü"
 ,
    --
-   -- Spezialtrennung
+   -- Ausdrücke für Alternativen
+   --
+   -- Eine Alternative beschreibt mehrdeutige Wortteile, die
+   -- unterschiedlich getrennt werden können.
+   --
+   -- Ausdrücke für Alternativen werden in begrenzende Zeichen
+   -- (Klammern) eingeschlossen.
+   cl_alt = V"alt_open" * V"alt_rule" * V"alt_close",
+   -- Teilausdrücke einer Alternative werden durch ein spezielles
+   -- Zeichen voneinander getrennt.  Beide Alternativen werden auf
+   -- Buchstabengleichheit geprüft.  Die Capture enthält abschließend
+   -- die Buchstaben der ersten Alternative.
+   alt_rule = (V"alt_1st" * V"alt_sep" * V"alt_2nd") / _is_equal_two_alternatives,
+   -- Die erste Alternative ist ein Teilwort.
+   alt_1st = V"part_word",
+   -- Die zweite Alternative ist ebenfalls ein Teilwort.  Diese Capture
+   -- wird später verworfen.
+   alt_2nd = V"part_word",
+   -- Zeichen, welches einen Ausdruck für Alternativen einführt.
+   alt_open = P"[",
+   -- Zeichen, welches einen Ausdruck für Alternativen abschließt.
+   alt_close = P"]",
+   -- Zeichen, welches Teilausdrücke einer Alternative voneinander
+   -- trennt.
+   alt_sep = P"/",
+   --
+   -- Ausdrücke für Spezialtrennungen
    --
    -- Spezialtrennungen (non-standard hyphenation) beschreiben
    -- Trennregeln, die über das bloße Einfügen eines Trennzeichens
@@ -221,11 +347,13 @@ local word = P{
    -- eingeschlossen in Klammern.  Die Capture enthält eine
    -- Zeichenkette, die der jeweils ungetrennten Spezialtrennung
    -- entspricht (z. B. 'ck' für die ck-Trennung).
+   --
+   -- Ausdrücke für Spezialtrennungen werden in begrenzende Zeichen
+   -- (Klammern) eingeschlossen.
    cl_nonstd = V"nonstd_open" * V"nonstd_rule" * V"nonstd_close",
    -- Aufzählung sämtlicher Spezialtrennungen.  Das Auftreten von
-   -- Spezialtrennungen wird in Tabelle <code>word_property</code>
-   -- gespeichert.
-   nonstd_rule = (V"ck" + V"bbb" + V"fff" + V"lll" + V"mmm" + V"nnn" + V"ppp" + V"rrr" + V"ttt" + V"nonstd_sss") / _word_prop_has_nonstd,
+   -- Spezialtrennungen wird als Worteigenschaft vermerkt.
+   nonstd_rule = (V"ck" + V"bbb" + V"fff" + V"lll" + V"mmm" + V"nnn" + V"ppp" + V"rrr" + V"ttt" + V"nonstd_sss") / _property_has_nonstd,
    -- ck-Trennung: Die Capture enthält die Zeichenfolge 'ck'.
    ck = C(P"ck") * V"nonstd_sep" * P"k" * V"hyphen" * P"k",
    -- Dreikonsonantenregel: Die Capture enthält die Zeichenfolge für die
@@ -239,100 +367,23 @@ local word = P{
    rrr = C(P"rr") * V"nonstd_sep" * P"rr" * V"hyphen" * P"r",
    ttt = C(P"tt") * V"nonstd_sep" * P"tt" * V"hyphen" * P"t",
    sss = C(P"ss") * V"nonstd_sep" * P"ss" * V"hyphen" * P"s",
-   -- Das Auftreten der Dreikonsonantenregel für 's' wird in Tabelle
-   -- <code>word_property</code> gespeichert.
-   nonstd_sss = V"sss" / _word_prop_has_nonstd_sss,
-   -- Klammer, die eine Spezialtrennung einführt.
+   -- Das Auftreten der Dreikonsonantenregel für 's' wird als
+   -- Worteigenschaft vermerkt.
+   nonstd_sss = V"sss" / _property_has_nonstd_sss,
+   -- Zeichen, welches einen Ausdruck für Spezialtrennungen einführt.
    nonstd_open = P"{",
-   -- Klammer, die eine Spezialtrennung abschließt.
+   -- Zeichen, welches einen Ausdruck für Spezialtrennungen abschließt.
    nonstd_close = P"}",
-   -- Trennzeichen innerhalb einer Spezialtrennung.
+   -- Zeichen, welches Teilausdrücke einer Spezialtrennung voneinander
+   -- trennt.
    nonstd_sep = P"/",
-   --
-   -- Alternative
-   --
-   -- Eine Alternative beschreibt mehrdeutige Wortteile, die
-   -- unterschiedlich getrennt werden können.  Die Capture enthält die
-   -- Zeichen der jeweils ersten Alternative ohne jegliche Trennzeichen.
-   --
-   -- Alternativen sind eingeschlossen in Klammern.
-   cl_alt = V"alt_open" * V"alt_rule" * V"alt_close",
-   -- Alternative Teilwörter werden durch ein spezielles Trennzeichen
-   -- voneinander getrennt.  Beide Alternativen werden auf
-   -- Buchstabengleichheit geprüft.  Die Capture enthält abschließend
-   -- die Buchstaben der ersten Alternative.
-   alt_rule = (V"alt_1st" * V"alt_sep" * V"alt_2nd") / _is_equal_two_alternatives,
-   -- Die erste Alternative ist ein Teilwort (mit Capture).
-   alt_1st = V"part_word",
-   -- Die zweite Alternative ist ebenfalls ein Teilwort.  Diese Capture
-   -- wird später verworfen.
-   alt_2nd = V"part_word",
-   -- Klammer, die eine Alternative einführt.
-   alt_open = P"[",
-   -- Klammer, die eine Alternative abschließt.
-   alt_close = P"]",
-   -- Trennzeichen innerhalb einer Alternative.
-   alt_sep = P"/",
-   --
-   -- Trennstellen
-   --
-   -- Die Markierung von Trennstellen erfolgt zunächst nach deren
-   -- morphologischer Struktur.  Wahlweise kann auch eine zusätzliche,
-   -- qualitative Bewertung erfolgen.
-   --
-   -- morphologisch
-   --
-   -- innerhalb von Wortstämmen oder vor Suffixen
-   hyphen_inner = V"hyphen_ch_inner"^1,
-   -- nach Präfixen oder Verbalpartikeln
-   hyphen_prefix = V"hyphen_ch_prefix"^1,
-   -- an Wortfugen
-   hyphen_compound = V"hyphen_ch_compound"^1,
-   -- Präfix eines zusammengesetzten Wortes
-   hyphen_compound_prefix = V"hyphen_ch_prefix" * V"hyphen_compound",
-   -- Suffix eines zusammengesetzten Wortes
-   hyphen_compound_suffix = V"hyphen_ch_inner" * V"hyphen_compound",
-   -- eine morphologische Markierung (Achtung: In der folgenden Regel
-   -- ist die Reihenfolge der Prüfung relevant.  Gemischte Trennzeichen
-   -- müssen vor reinen Trennzeichen geprüft werden.)
-   hyphen_morph =
-      V"hyphen_compound_prefix"
-      + V"hyphen_compound_suffix"
-      + V"hyphen_inner"
-      + V"hyphen_prefix"
-      + V"hyphen_compound",
-   --
-   -- qualitativ
-   --
-   -- eine Bewertung
-   hyphen_quality = V"hyphen_ch_quality" * V"hyphen_ch_quality"^-2,
-   -- eine optionale Bewertung
-   hyphen_opt_quality = V"hyphen_quality"^-1,
-   --
-   -- unkategorisiert
-   --
-   hyphen_uncategorized = V"hyphen_ch_uncategorized",
-   --
-   -- Eine beliebige Trennstelle ist entweder unkategorisiert oder
-   -- morphologisch markiert mit optional folgender Bewertung.  Die
-   -- Bewertung kann auch alleinstehen.
-   hyphen = V"hyphen_uncategorized"
-      + V"hyphen_morph" * V"hyphen_opt_quality"
-      + V"hyphen_quality"
-,
-   --
-   -- Ein beliebiges, optionales Trennzeichen.
-   ophyphen = V"hyphen"^-1,
-   --
-   -- Die folgenden Trennzeichen werden verwendet:
-   hyphen_ch_inner = P"-",
-   hyphen_ch_prefix = P"|",
-   hyphen_ch_compound = P"=",
-   hyphen_ch_quality = P".",
-   hyphen_ch_uncategorized = P"·",
 }
---
---- Ermittle Eigenschaften eines Wortes.
+
+
+
+--- Prüfe String gegen Wortgrammatik.
+-- Falls der String ein zulässiges Wort darstellt, wird eine Tabelle mit
+-- Eigenschaften des Wortes zurückgegeben.
 --
 -- @param rawword unbehandeltes Wort
 --
@@ -340,26 +391,26 @@ local word = P{
 -- hat;<br /> eine Tabelle mit Eigenschaften des betrachteten Wortes,
 -- sonst.
 local function parse_word(rawword)
-   -- Verwirf eventuelle weitere Captures.
-   return ( word:match(rawword) )
+   return word:match(rawword)
 end
 M.parse_word = parse_word
 
 
 
---- Normalisiere ein Wort.  Resultat ist ein Wort in einem für Patgen
---- geeigneten Format, falls das Wort zulässig ist.  Sämtliche
---- Trennzeichen werden durch '<code>-</code>' ersetzt.
---- Spezialtrennungen und Alternativen werden aufgelöst.
+--- Normalisiere ein Wort.
+--  Resultat ist ein Wort in einem für Patgen geeigneten Format, falls
+-- das Wort zulässig ist.  Sämtliche Trennzeichen werden durch
+-- '<code>-</code>' ersetzt.  Spezialtrennungen und Alternativen werden
+-- aufgelöst.
 --
 -- @param rawword unbehandeltes Wort
 --
 -- @return <code>nil</code>, falls das Wort eine unzulässige Struktur
--- hat;<br /> <code>word, props</code>, sonst.  <code>word</code> ist
--- das normalisierte Wort, <code>props</code> ist eine Tabelle mit
+-- hat;<br /> <code>word, props</code>, sonst.<br /> <code>word</code>
+-- ist das normalisierte Wort, <code>props</code> ist eine Tabelle mit
 -- Eigenschaften des betrachteten Wortes.
 local function normalize_word(rawword)
-   -- vorbereitende Prüfung der Wortstruktur und Worteigenschaften
+   -- Prüfung der Wortstruktur und Ermittlung der Worteigenschaften.
    local props = parse_word(rawword)
    -- Hat das Wort eine zulässige Struktur?
    if not props then return nil end
@@ -388,7 +439,7 @@ M.normalize_word = normalize_word
 -- @param rawword Wort (normiert oder unbehandelt)
 --
 -- @return <code>nil, msg</code>, falls das Wort eine unzulässige
--- Struktur hat;<br /> <code>word, props</code>, sonst.
+-- Struktur hat;<br /> <code>word, props</code>, sonst.<br />
 -- <code>msg</code> ist ein String, der die Unzulässigkeit näher
 -- beschreibt.  <code>word</code> ist das normalisierte Wort.
 -- <code>props</code> ist eine Tabelle mit Eigenschaften des
