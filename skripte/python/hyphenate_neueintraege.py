fdcf8baf52aff7e0da00c43e7a5341129d6c7de2
Guenter Milde <milde@users.sf.net>
2014-07-05 17:07:41 +0200

    Pure-Python-Implementation des TeX-Trennalgorithmus.
    Adaption eines Skripts von Ned Batchelder.
    Anwendung zur kategorisierten Trennung mit unseren Patgen-Mustern.

---
+++ skripte/python/hyphenate_neueintraege.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+# :Copyright: © 2014 Günter Milde.
+#             Released without warranty under the terms of the
+#             GNU General Public License (v. 2 or later)
+# :Id: $Id:  $
+
+# hyphenate_neueintraege.py: Versuche kategorisierte Trennung
+# über "hyphenation"-Algorithmus und patgen-patterns.
+# ============================================================
+
+
+
+import sys, codecs, glob, copy
+from werkzeug import WordFile, WordEntry, join_word, toggle_case, sortkey_duden
+from abgleich_neueintraege import print_proposal
+from hyphenation import Hyphenator
+
+
+# Konfiguration
+# -------------
+
+# Pfad zur Datei mit den neu einzutragenden Wörtern::
+
+neuwortdatei = "spell/zusatz-de-1996-aspell-compact"
+# neuwortdatei = "spell/DDR.txt"
+
+
+# Die neuesten Pattern-Dateien welche über die "make" Ziele
+# `make pattern-refo`, `make major pattern-refo`, `make fugen pattern-refo`
+# im Wurzelverzeichnis der wortliste generiert werden::
+        
+pfile_all = glob.glob('../../dehyphn-x/dehyphn-x-*.pat')[-1]
+pfile_major = glob.glob('../../dehyphn-x-major/dehyphn-x-major-*.pat')[-1]
+pfile_fugen = glob.glob('../../dehyphn-x-fugen/dehyphn-x-fugen-*.pat')[-1]
+
+
+
+# Trenner-Instanzen::
+
+h_all = Hyphenator(pfile_all)
+h_major = Hyphenator(pfile_major)
+h_fugen = Hyphenator(pfile_fugen)
+        
+
+# Trenne mit Hyphenator::
+
+def trenne(entry):
+    key = entry[0]
+    word = u'='.join([u'<'.join([h_all.hyphenate_word(part, '-') 
+                                 for part in h_major.split_word(teilwort)])
+                      for teilwort in h_fugen.split_word(key)])
+    return WordEntry(key + u';' + word)
+
+if __name__ == '__main__':
+
+    # sys.stdout mit UTF8 encoding.
+    sys.stdout = codecs.getwriter('UTF-8')(sys.stdout)
+
+    neue = []
+
+# Erstellen der neuen Einträge::
+
+    proposals = [WordEntry(line.decode('utf8').strip())
+                 for line in open(neuwortdatei)
+                 if not line.startswith('#')]
+
+    for newentry in proposals:
+
+# Trennen::
+    
+        entry = trenne(copy.copy(newentry))
+        if entry:
+            neue.append(entry)
+
+
+# Vergleich mit Original::
+
+    alle_neuen = dict((entry[0].lower(), entry) for entry in neue)
+
+    identische = {}
+    for proposal in proposals:
+        key = proposal[0].lower()
+        newentry = alle_neuen.get(key)
+        if proposal == newentry:
+            identische[key] = proposal
+        else:
+            if newentry:
+                newentry.proposal = proposal
+                
+# Ausgabe::
+
+    print u'\n# identisch rekonstruiert:'
+    for entry in sorted(identische.values(), key=sortkey_duden):
+        print unicode(entry)
+
+    print u'\n# mit Pattern getrennt'
+    for entry in neue:
+        print_proposal(entry)
---
+++ skripte/python/hyphenation.py
@@ -0,0 +1,173 @@
+#!/usr/bin/env python
+# -*- coding: utf8 -*-
+
+# Original from http://nedbatchelder.com/code/modules/hyphenate.py
+
+u""" Hyphenation, using a pure Python implementation Frank Liang's algorithm.
+
+    This module provides a class to hyphenate words.
+
+    `Hyphenator.split_word(word)` takes a string (the word), and returns a
+    list of parts that can be separated by hyphens.
+
+    hyphenator = Hyphenator(pattern_file)
+    >>> hyphenator.split_word(u"hyphenation")
+    [u'hy', u'phen', u'ation']
+    >>> hyphenator.hyphenate_word(u"supercalifragilisticexpialidocious", '-')
+    u'su-per-cal-ifrag-ilis-tic-ex-pi-ali-do-cious'
+    >>> hyphenator.hyphenate_word(u"project")
+    u'project'
+
+    version 1: Ned Batchelder, July 2007.
+    This Python code is in the public domain.
+
+    version 2: Internationalization (external pattern files, Unicode)
+    © 2013 Günter Milde
+"""
+
+import re, optparse
+
+__version__ = '2.0 2014-07-04'
+
+class Hyphenator:
+    def __init__(self, pattern_file, exceptions=''):
+        self.tree = {}
+        for pattern in self.yield_patterns(pattern_file):
+            self._insert_pattern(pattern)
+
+        self.exceptions = {}
+        for ex in exceptions.split():
+            # Convert the hyphenated pattern into a point array for use later.
+            self.exceptions[ex.replace(u'-', u'')] = [0] + [ int(h == u'-')
+                                            for h in re.split(ur"[^-]", ex) ]
+
+    def yield_patterns(self, path, invalid_chars = '%\\{}', encoding='utf8'):
+        """
+        Yield hyphenation patterns from a file.
+
+        Pattern file format: As used by TEX
+        * one pattern per line,
+        * every line containing one of the characters in
+          the string `invalid_chars` (comments and TeX macros) is discarded,
+        * file encoding in argument `encoding` (default 'utf8').
+        """
+        # TODO: process OpenOffice hyphenation files?
+        # (Suffix '.dic', encoding specified in first line of file).
+        lines = open(path)
+        for line in lines:
+            for c in invalid_chars:
+                if c in line:
+                    line = ''
+                    continue
+            line = line.decode(encoding).strip()
+            if line:
+                yield line
+        lines.close()
+
+    def _insert_pattern(self, pattern):
+        # Convert the a pattern like 'a1bc3d4' into a string of chars 'abcd'
+        # and a list of points [ 1, 0, 3, 4 ].
+        chars = re.sub(u'[0-9]', u'', pattern)
+        points = [ int(d or 0) for d in re.split(u'[^0-9]', pattern) ]
+
+        # Insert the pattern into the tree.  Each character finds a dict
+        # another level down in the tree, and leaf nodes have the list of
+        # points.
+        t = self.tree
+        for c in chars:
+            if c not in t:
+                t[c] = {}
+            t = t[c]
+        t[None] = points
+
+    def split_word(self, word, lmin=2, rmin=2):
+        """ Given a word, returns a list of pieces, broken at the possible
+            hyphenation points.
+        """
+        # Short words aren't hyphenated.
+        if len(word) <= lmin + rmin:
+            return [word]
+        # If the word is an exception, get the stored points.
+        if word.lower() in self.exceptions:
+            points = self.exceptions[word.lower()]
+        else:
+            work = '.' + word.lower() + '.'
+            points = [0] * (len(work)+1)
+            for i in range(len(work)):
+                t = self.tree
+                for c in work[i:]:
+                    if c in t:
+                        t = t[c]
+                        if None in t:
+                            p = t[None]
+                            for j in range(len(p)):
+                                points[i+j] = max(points[i+j], p[j])
+                    else:
+                        break
+            # No hyphens in the first two chars or the last two.
+            for i in range(lmin):
+                points[i+1] = 0
+            for i in range(rmin):
+                points[-2-i] = 0
+            # points[1] = points[2] = points[-2] = points[-3] = 0
+
+        # Examine the points to build the pieces list.
+        pieces = ['']
+        for c, p in zip(word, points[2:]):
+            pieces[-1] += c
+            if p % 2:
+                pieces.append('')
+        return pieces
+
+    def hyphenate_word(self, word, hyphen=u'­'):
+        """ Return `word` with (soft-)hyphens at the possible
+            hyphenation points.
+        """
+        return hyphen.join(self.split_word(word))
+
+
+pattern_file = 'en-US.pat'
+# pattern_file = '../../dehyphn-x/dehyphn-x-2014-06-25.pat'
+# pattern_file = '../../dehyphn-x-fugen/dehyphn-x-fugen-2014-07-01.pat'
+
+
+if __name__ == '__main__':
+
+    usage = u'%prog [options] [words to be hyphenated]\n\n' + __doc__
+
+    parser = optparse.OptionParser(usage=usage)
+    parser.add_option('-f', '--pattern-file', dest='pattern_file',
+                      help='Pattern file, Default "en-US.pat"',
+                      default='en-US.pat')
+    parser.add_option('-e', '--exception-file', dest='exception_file',
+                      help='File of hyphenated words (exceptions), '
+                      'Default None', default='')
+    parser.add_option('-i', '--input-file', dest='input_file',
+                      help='Eingabedatei (ein Wort/Zeile)',
+                      default='')
+    (options, args) = parser.parse_args()
+
+
+
+    if options.exception_file:
+        ex_file = open(exception_file)
+        exceptions = ex_file.read().decode('utf8')
+        ex_file.close()
+    else:
+        exceptions = ''
+    if len(args) == 0: # self test
+        exceptions = u"""
+            as-so-ciate as-so-ciates dec-li-na-tion oblig-a-tory
+            phil-an-thropic present presents project projects reci-procity
+            re-cog-ni-zance ref-or-ma-tion ret-ri-bu-tion ta-ble
+            """
+    hyphenator = Hyphenator(options.pattern_file, exceptions)
+    del exceptions
+
+    if len(args) > 0:
+        words = [word.decode('utf8') for word in args]
+        for word in words:
+            print hyphenator.hyphenate_word(word)
+    else:
+        import doctest
+        doctest.testmod(verbose=True)
